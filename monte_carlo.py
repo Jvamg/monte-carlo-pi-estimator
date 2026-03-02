a582098cee85a7faa161d5a8772acbc3f3c82fe6
Jvamg <90657955+Jvamg@users.noreply.github.com>
2026-03-01 23:14:52 -0300

    Adding: numpyWay function

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,5 +1,6 @@
 import random
 import time
+import numpy as np
 
 
 def forWay(n):
@@ -20,5 +21,25 @@ def forWay(n):
     print(time_end - time_start)
 
 
-n = 1000000
-forWay(n)
+def numpyWay(n):
+    time_start = time.perf_counter()
+    count = 0
+    x = np.random.uniform(-1, 1, n)
+    y = np.random.uniform(-1, 1, n)
+
+    z = x**2 + y**2
+    count = np.sum(z <= 1)
+
+    pi = 4 * count / n
+
+    time_end = time.perf_counter()
+
+    print(pi)
+    print(time_end - time_start)
+
+
+if __name__ == '__main__':
+    n = 10000000
+
+    forWay(n)
+    numpyWay(n)
